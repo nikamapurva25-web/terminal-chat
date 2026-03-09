2dc7f9f1797280fcb65a7636edb295c5d4691f8f
Apurva Nikam <nikamapurva25@gmail.com>
2026-03-09 22:09:37 +0530

    fix socket server port

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 const io = require("socket.io-client");
 const readline = require("readline");
+const chalk = require("chalk").default;
 
-const socket = io("http://localhost:3000");
+const socket = io("https://terminal-chat-730t.onrender.com");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,10 +11,15 @@ const rl = readline.createInterface({
 
 const username = process.argv[2] || "user";
 
+socket.on("connect", () => {
+  console.log(chalk.green("Connected to chat server"));
+  socket.emit("join", username);
+});
+
 socket.on("chat", (msg) => {
   console.log(msg);
 });
 
 rl.on("line", (input) => {
-  socket.emit("chat", username + ": " + input);
+  socket.emit("chat", chalk.cyan(username) + ": " + input);
 });
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const { Server } = require("socket.io");
 
-const io = new Server(3000, {
+const PORT = process.env.PORT || 3000;
+
+const io = new Server(PORT, {
   cors: { origin: "*" }
 });
 
-console.log("Chat server running on port 3000");
+let users = {};
+
+console.log("Chat server running");
 
 io.on("connection", (socket) => {
 
+  socket.on("join", (username) => {
+    users[socket.id] = username;
+    io.emit("chat", `🔔 ${username} joined the chat`);
+  });
+
   socket.on("chat", (msg) => {
     io.emit("chat", msg);
+  });
+
+  socket.on("disconnect", () => {
+    const username = users[socket.id];
+    if (username) {
+      io.emit("chat", `❌ ${username} left the chat`);
+      delete users[socket.id];
+    }
   });
 
 });
